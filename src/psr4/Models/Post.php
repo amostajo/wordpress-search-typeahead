b3bc71374d9c7a8cb89f77941ae9743f6abfc22b
Alejandro Mostajo <amostajo@amsgames.com>
2015-11-19 20:12:22 -0600

    Refactoring

--- src/psr4/Models/Post.php
+++ src/psr4/Models/Post.php
@@ -15,120 +15,120 @@ use Amostajo\LightweightMVC\Traits\FindTrait as FindTrait;
  */
 class Post extends Model
 {
-	use FindTrait;
+    use FindTrait;
 
-	/**
-	 * Aliases
-	 * @var array
-	 * @since 1.0
-	 */
-	protected $aliases = [
-		'excerpt'			=> 'func_get_excerpt',
-		'image_id' 			=> 'func_get_image_id',
-		'image_url' 		=> 'func_get_image_url',
-		'thumb_image_url' 	=> 'func_get_thumb_image_url',
-		'permalink' 		=> 'func_get_permalink',
-	];
+    /**
+     * Aliases
+     * @var array
+     * @since 1.0
+     */
+    protected $aliases = [
+        'excerpt'           => 'func_get_excerpt',
+        'image_id'          => 'func_get_image_id',
+        'image_url'         => 'func_get_image_url',
+        'thumb_image_url'   => 'func_get_thumb_image_url',
+        'permalink'         => 'func_get_permalink',
+    ];
 
-	/**
-	 * Hidden properties
-	 * @var array
-	 * @since 1.0
-	 */
-	protected $hidden = [
-		'post_content', 'guid', 'post_excerpt', 'post_author', 'image_url',
-		'post_date_gmt', 'to_ping', 'pinged', 'comment_status', 'ping_status',
-		'post_password', 'post_name', 'post_modified', 'post_modified_gmt',
-		'post_content_filtered', 'post_parent', 'menu_order', 'post_mime_type',
-		'comment_count', 'filter', 'ancestors', 'image_id',
-	];
+    /**
+     * Hidden properties
+     * @var array
+     * @since 1.0
+     */
+    protected $hidden = [
+        'post_content', 'guid', 'post_excerpt', 'post_author', 'image_url',
+        'post_date_gmt', 'to_ping', 'pinged', 'comment_status', 'ping_status',
+        'post_password', 'post_name', 'post_modified', 'post_modified_gmt',
+        'post_content_filtered', 'post_parent', 'menu_order', 'post_mime_type',
+        'comment_count', 'filter', 'ancestors', 'image_id',
+    ];
 
-	/**
-	 * Returns image url.
-	 * @since 1.0
-	 *
-	 * @return string
-	 */
-	protected function get_image_id()
-	{
-		return get_post_thumbnail_id( $this->ID );
-	}
+    /**
+     * Returns image url.
+     * @since 1.0
+     *
+     * @return string
+     */
+    protected function get_image_id()
+    {
+        return get_post_thumbnail_id( $this->ID );
+    }
 
-	/**
-	 * Returns image url.
-	 * @since 1.0
-	 *
-	 * @return string
-	 */
-	protected function get_image_url()
-	{
-		$id = $this->image_id;
-		if ( $id ) {
-			return wp_get_attachment_url( $id );
-		}
-		return;
-	}
+    /**
+     * Returns image url.
+     * @since 1.0
+     *
+     * @return string
+     */
+    protected function get_image_url()
+    {
+        $id = $this->image_id;
+        if ( $id ) {
+            return wp_get_attachment_url( $id );
+        }
+        return;
+    }
 
-	/**
-	 * Returns thumbnail size image url.
-	 * @since 1.0
-	 *
-	 * @return string
-	 */
-	protected function get_thumb_image_url()
-	{
-		$url = $this->image_url;
-		if ( $url ) {
-			return resize_image(
-				$url,
-				120,
-				120
-			);
-		}
-		return;
-	}
+    /**
+     * Returns thumbnail size image url.
+     * @since 1.0
+     *
+     * @return string
+     */
+    protected function get_thumb_image_url()
+    {
+        $url = $this->image_url;
+        if ( $url ) {
+            return resize_image(
+                $url,
+                120,
+                120
+            );
+        }
+        return;
+    }
 
-	/**
-	 * Returns permalink.
-	 * @since 1.0
-	 *
-	 * @return string
-	 */
-	protected function get_permalink()
-	{
-		return get_permalink( $this->ID );
-	}
+    /**
+     * Returns permalink.
+     * @since 1.0
+     *
+     * @return string
+     */
+    protected function get_permalink()
+    {
+        return get_permalink( $this->ID );
+    }
 
-	/**
-	 * Returns excerpt.
-	 * @since 1.0
-	 *
-	 * @return string
-	 */
-	protected function get_excerpt()
-	{
-		if ( $this->post_excerpt )
-			return $this->post_excerpt;
-		return $this->excerpt( $this->post_content );
-	}
+    /**
+     * Returns excerpt.
+     * @since 1.0
+     *
+     * @return string
+     */
+    protected function get_excerpt()
+    {
+        if ( $this->post_excerpt )
+            return $this->post_excerpt;
+        return $this->excerpt( $this->post_content );
+    }
 
-	/**
-	 * Excerpts a string.
-	 *
-	 * @param string $string     String to excerpt.
-	 * @param int    $char_limit Char limit to excerpt.
-	 * @param string $more       String to show add the end if string surpasses limit.
-	 *
-	 * @return string
-	 */
-	private function excerpt( $string, $char_limit = 15, $more = '...' )
-	{
-		$string = strip_tags( $string );
-		$return = preg_replace( "/^\W*((\w[\w'-]*\b\W*){1," . $char_limit . "}).*/ms", '\\1', $string );
-		$return = str_replace( "\n", "", $return );
-		if( str_word_count( $string ) !== str_word_count( $return ) ) {
-		  $return .= ' ' . $more;
-		}
-		return $return;
-	}
+    /**
+     * Excerpts a string.
+     *
+     * @param string $string     String to excerpt.
+     * @param int    $char_limit Char limit to excerpt.
+     * @param string $more       String to show add the end if string surpasses limit.
+     *
+     * @return string
+     */
+    private function excerpt( $string, $char_limit = 15, $more = '...' )
+    {
+        $string = strip_tags( $string );
+        $return = preg_replace( "/^\W*((\w[\w'-]*\b\W*){1," . $char_limit . "}).*/ms", '\\1', $string );
+        $return = str_replace( "\n", "", $return );
+        if( str_word_count( $string ) !== str_word_count( $return ) ) {
+			$return .= ' ' . $more;
+        }
+        return $return;
+    }
 }
--- src/psr4/Typeahead.php
+++ src/psr4/Typeahead.php
@@ -25,55 +25,55 @@ class Typeahead extends Addon
         wp_enqueue_script( 'wp-typeahead' );
     }
 
-	/**
-	 * Wordpress HOOKS.
-	 * @since 1.0
-	 */
-	public function init()
-	{
-		add_action( 'init', [ &$this, 'config' ] );
-		add_action( 'wp_ajax_nopriv_addon_typeahead', [ &$this, 'typeahead_json' ] );
-		add_action( 'wp_ajax_addon_typeahead', [ &$this, 'typeahead_json' ] );
-		add_filter( 'addon_typeahead_post', [ &$this, 'typeahead_filter' ], 100, 2 );
-	}
+    /**
+     * Wordpress HOOKS.
+     * @since 1.0
+     */
+    public function init()
+    {
+        add_action( 'init', [ &$this, 'config' ] );
+        add_action( 'wp_ajax_nopriv_addon_typeahead', [ &$this, 'typeahead_json' ] );
+        add_action( 'wp_ajax_addon_typeahead', [ &$this, 'typeahead_json' ] );
+        add_filter( 'addon_typeahead_post', [ &$this, 'typeahead_filter' ], 100, 2 );
+    }
 
-	/**
-	 * Displayes queried POSTS in JSON format.
-	 * @since 1.0
-	 */
-	public function typeahead_json()
-	{
-		$this->mvc->call( 'TypeaheadController@json' );
-	}
+    /**
+     * Displayes queried POSTS in JSON format.
+     * @since 1.0
+     */
+    public function typeahead_json()
+    {
+        $this->mvc->call( 'TypeaheadController@json' );
+    }
 
-	/**
-	 * Filters post and transforms it into a model.
-	 * @since 1.0
-	 *
-	 * @param object $post WP_Post
-	 *
-	 * @return object Model
-	 */
-	public function typeahead_filter( $post )
-	{
-		return $this->mvc->action( 'TypeaheadController@filter', $post );
-	}
+    /**
+     * Filters post and transforms it into a model.
+     * @since 1.0
+     *
+     * @param object $post WP_Post
+     *
+     * @return object Model
+     */
+    public function typeahead_filter( $post )
+    {
+        return $this->mvc->action( 'TypeaheadController@filter', $post );
+    }
 
-	/**
-	 * Addon configuration.
+    /**
+     * Addon configuration.
      * Registers dependencies.
-	 * @since 1.0
-	 */
-	public function config()
-	{
-		wp_register_script(
+     * @since 1.0
+     */
+    public function config()
+    {
+        wp_register_script(
             'typeahead-all',
             plugins_url( '../../assets/build/typeahead-all.min.js' , __FILE__ ),
             [ 'jquery' ],
             '1.0.0',
             true
         );
-		wp_register_script(
+        wp_register_script(
             'wp-typeahead',
             plugins_url( '../../assets/build/wp-typeahead.min.js' , __FILE__ ),
             [ 'typeahead-all' ],
@@ -86,5 +86,5 @@ class Typeahead extends Addon
             [],
             '1.0.0'
         );
-	}
+    }
 }
